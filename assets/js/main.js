d88be1ef54bf3405ec990aba966ae3fa2d7195b0
Arjun Suthaharan <arjunsutha@gmail.com>
2023-02-01 13:36:35 -0500

    Fixed navbar scaling problems on mobile resolutions, added reference to original template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 /*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Built off of Miniport by HTML5 UP
 */
 
 (function($) {
